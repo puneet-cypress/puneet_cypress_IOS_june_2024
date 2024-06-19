424d596a48fcbcbc4cb96c09c95c9617622b8b88
Puneet Aneja <puneetaneja@Puneets-Air.attlocal.net>
2024-06-19 11:03:39 -0700

    added some comments on puneetfouth branch

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -11,8 +11,7 @@ beforeEach (() => {
 
   it('verif courses on the home page', () => {
   cy.get('dt').eq(0).contains("4 Courses")
-  cy.get('dt').eq(1).contains("25+ Lessons")
-  cy.get('dt').eq(2).contains("Free and Open Source")
+  //adding a new line
     
 })
 });
